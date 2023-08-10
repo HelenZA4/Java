b3daeac01d27ade64ae1d3ef856c8e789463ca6f
Elena Zaytseva <139309253+HelenZA4@users.noreply.github.com>
2023-08-10 22:05:01 +0700

    Update Average.java

--- Average.java
+++ Average.java
@@ -7,33 +7,33 @@ public class Average {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите начальное число диапазона: ");
-        int a = scanner.nextInt();
+        int start = scanner.nextInt();
 
-        if (a <= 1) {
+        if (start <= 1) {
             System.out.println("Начальное число диапазона должно быть больше 1.");
             return;
         }
 
         System.out.println("Введите конечное число диапазона: ");
-        int x = scanner.nextInt();
+        int end = scanner.nextInt();
 
-        if (x <= a) {
+        if (end <= start) {
             System.out.println("Конечное число диапазона должно быть больше начального числа.");
             return;
         }
 
-        double averageAll = getAverage(a, x);
-        System.out.println("Среднее арифметическое всех чисел в диапазоне [" + a + ", " + x + "] равно: " + averageAll);
+        double averageAll = getAverage(start, end);
+        System.out.println("Среднее арифметическое всех чисел в диапазоне [" + start + ", " + end + "] равно: " + averageAll);
 
-        double averageEven = getAverageEven(a, x);
-        System.out.println("Среднее арифметическое четных чисел в диапазоне [" + a + ", " + x + "] равно: " + averageEven);
+        double averageEven = getAverageEven(start, end);
+        System.out.println("Среднее арифметическое четных чисел в диапазоне [" + start + ", " + end + "] равно: " + averageEven);
     }
 
-    public static double getAverage(int a, int x) {
+    public static double getAverage(int start, int end) {
         int sum = 0;
         int count = 0;
 
-        for (int i = a; i <= x; i++) {
+        for (int i = start; i <= end; i++) {
             sum += i;
             count++;
         }
@@ -41,11 +41,11 @@ public class Average {
         return (double) sum / count;
     }
 
-    public static double getAverageEven(int a, int x) {
+    public static double getAverageEven(int start, int end) {
         int sumEven = 0;
         int countEven = 0;
 
-        for (int i = a; i <= x; i++) {
+        for (int i = start; i <= end; i++) {
             if (i % 2 == 0) {
                 sumEven += i;
                 countEven++;
